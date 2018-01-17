47af7b783f5676a2f88ca242ebb253535dbe33ac
Marshall <xtremebass2k@gmail.com>
2018-01-16 23:48:24 -0800

    turns out simplecache is not great for production enviroments

--- benwaonline_auth/bwoauth/__init__.py
+++ benwaonline_auth/bwoauth/__init__.py
@@ -1,8 +1,15 @@
+import os
 from flask import Blueprint
 from werkzeug.contrib.cache import SimpleCache
-from benwaonline_auth.models import Client
+from pymemcache.client.base import Client
+# from benwaonline_auth.models import Client
 
-cache = SimpleCache()
+if os.getenv('FLASK_CONFIG') == 'prod':
+    addr = os.getenv('MEMCACHED_ADDR')
+    cache = Client([addr])
+else:
+    cache = SimpleCache()
+
 auth = Blueprint('auth', __name__)
 
 from benwaonline_auth.bwoauth import views, core
